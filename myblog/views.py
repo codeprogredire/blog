dfaa43665798e6d4f779d5f80bd1df5a3c8e5a6d
codeprogredire <skislay7@gmail.com>
2019-05-22 14:43:23 +0530

    First commit!

--- myblog/views.py
+++ myblog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.utils import timezone
 # Create your views here.
+def post_list(request):
+    posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request,'myblog/post_list.html',{'posts':posts})
+
+def about(request):
+    return render(request,'myblog/about.html',{})
+
+
+def contact(request):
+    return render(request,'myblog/contact.html',{})
+
+    
